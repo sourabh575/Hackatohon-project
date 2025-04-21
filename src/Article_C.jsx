69337ee9d209ec7e137aae13f9bc3e3acb3f4582
shivangigond <162681936+gondsachin40@users.noreply.github.com>
2025-04-21 12:54:40 +0530

    third commit

--- src/Article_C.jsx
+++ src/Article_C.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import "./CSS/Article.css";
-import topicsInfo from "./DATA/C"
-function Article() { 
-  const [selectedTopic, setSelectedTopic] = useState("datatype");
+import topicsInfo from "./DATA/C";
 
-  const selectedTopicInfo = topicsInfo.find((topic) => topic.topic === selectedTopic);
+function Article() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const selectedTopicInfo = topicsInfo[selectedIndex];
+
+  const handleNext = () => {
+    if (selectedIndex < topicsInfo.length - 1) {
+      setSelectedIndex(selectedIndex + 1);
+    }
+  };
 
   return (
     <div className="article-container">
-      {/* side content yaha dikhega*/}
+      {/* Side content */}
       <div className="contents">
         <h2>Contents</h2>
         <ul>
-          {topicsInfo.map((topic) => (
+          {topicsInfo.map((topic, index) => (
             <li
               key={topic.topic}
-              className={selectedTopic === topic.topic ? "active" : ""}
-              onClick={() => setSelectedTopic(topic.topic)}
+              className={selectedIndex === index ? "active" : ""}
+              onClick={() => setSelectedIndex(index)}
             >
               {topic.topic}
             </li>
@@ -24,9 +31,19 @@ function Article() {
         </ul>
       </div>
 
+      {/* Main content */}
       <div className="content-display">
         <h1>{selectedTopicInfo.topic}</h1>
         <p>{selectedTopicInfo.info}</p>
+
+        {/* Next Button */}
+        <button
+          className="next-button"
+          onClick={handleNext}
+          disabled={selectedIndex === topicsInfo.length - 1}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
